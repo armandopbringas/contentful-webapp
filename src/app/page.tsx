f604980540922484edf00d56832e9f8ea88d9e0d
armandopbringas <bringas.armandop@gmail.com>
2024-06-08 16:33:04 -0600

    starting project: contentful configs & connection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,37 @@
-import Image from "next/image";
+import * as contentful from "contentful";
 import styles from "./page.module.css";
 
-export default function Home() {
+const client = contentful.createClient({
+  space: process.env.CONTENTFUL_SPACE_ID = "5bpk7lniggsc" as string,
+  accessToken: process.env.CONTENFUL_ACCESS_TOKEN = "BO3rN8mkhPK0S8WhkbaSYyc5uIiATDrV8wUR-o8IpV8" as string
+});
+
+interface HomeProps {
+  title: string;
+  description: string;
+  url: string;
+}
+
+async function getData(): Promise<HomeProps> {
+  const result = await client.getEntry(process.env.ENTRY_ID = "5qDnYeVggu7v7WoDgvgfUV" as string);
+  return {
+    title: result.fields.title || "",
+    description: result.fields.description || "",
+    url: result.fields.url || ""
+  };
+}
+
+export default async function Home() {
+  const { title, description, url } = await getData();
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <h1>{title}</h1>
+        <p>{description}</p>
+        <a href={url}>url</a>
       </div>
     </main>
   );
 }
+
